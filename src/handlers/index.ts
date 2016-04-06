64e1aa28b8aca2f9eb6da02f47cfad5da9cb822d
nskazki <nskazki@gmail.com>
2016-04-06 15:01:51 +0300

    made import more explicitly - Wallaby failed to cope with the interpretation of the dynamic dependencies import

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,17 +1,15 @@
 'use strict'
 
 import { IFindAll, IProcessOne, IHandler } from '../interfaces'
-import { resolve } from 'path'
-import { readdirSync, statSync  } from 'fs'
 
-const isFile = (path: string) => statSync(path).isFile()
-const isHandlerFile = (path: string) => /\w+Handler\.(ts|js)$/.test(path)
+import jsonHandler from './jsonHandler'
+import lineHandler from './lineHandler'
+import stringHandler from './stringHandler'
+import tabHandler from './tabHandler'
 
-const handlers = readdirSync(__dirname)
-  .map(file => resolve(__dirname, file))
-  .filter(path => isFile(path))
-  .filter(path => isHandlerFile(path))
-  .map(path => <IHandler> require(path))
+const handlers = [
+  jsonHandler, lineHandler,
+  stringHandler, tabHandler ]
 
 export const findAll: IFindAll = function(fmtStr) {
   const psInfo = handlers

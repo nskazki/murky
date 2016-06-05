68fbdffa0dff79263f94dd721bf82a8ffd52d36b
nskazki <nskazki@gmail.com>
2016-06-05 04:43:20 +0300

    replaced custom check in helpers/pInfo by assert

--- src/helpers/pInfo.ts
+++ src/helpers/pInfo.ts
@@ -2,6 +2,7 @@
 
 import { IPInfo, IHandler } from '../interfaces'
 import { last } from 'lodash'
+import assert = require('assert')
 
 export class PInfoBase implements IPInfo {
   placeholder: string
@@ -12,38 +13,33 @@ export class PInfoBase implements IPInfo {
   length: number
 
   protected _selfCheck() {
-    if (this.placeholder.length === 0) {
-      throw new Error(`PInfoBase: internal problem: placeholder length must be greater then zero\
+    assert.ok(this.placeholder.length !== 0,
+      `PInfoBase: internal problem: placeholder length must be greater then zero\
         \n\t placeholder: ${this.placeholder}\
         \n\t placeholder.length: ${this.placeholder.length}`)
-    }
 
-    if (this.handlerName.length === 0) {
-      throw new Error(`PInfoBase: internal problem: handlerName length must be greater then zero\
+    assert.ok(this.handlerName.length !== 0,
+      `PInfoBase: internal problem: handlerName length must be greater then zero\
         \n\t handlerName: ${this.handlerName}\
         \n\t handlerName.length: ${this.handlerName.length}`)
-    }
 
-    if (this.indexStart < 0 || this.indexEnd < 0) {
-      throw new Error(`PInfoBase: internal problem: indexEnd and indexStart must be greater then zero\
+    assert.ok(this.indexStart >= 0 && this.indexEnd >= 0,
+      `PInfoBase: internal problem: indexEnd and indexStart must be greater then zero\
         \n\t indexStart; ${this.indexStart}\
         \n\t indexEnd: ${this.indexEnd}`)
-    }
 
-    if (this.indexStart > this.indexEnd) {
-      throw new Error(`PInfoBase: internal problem: indexEnd must be less then indexEnd\
+    assert.ok(this.indexStart < this.indexEnd,
+      `PInfoBase: internal problem: indexEnd must be less then indexEnd\
         \n\t indexStart: ${this.indexStart}\
         \n\t indexEnd: ${this.indexEnd}`)
-    }
 
-    if (this.length !== this.placeholder.length) {
-      throw new Error(`PInfoBase: internal problem: placeholder length must be equal to the length\
+    assert.ok(this.length === this.placeholder.length,
+      `PInfoBase: internal problem: placeholder length must be equal to the length\
         \n\t placeholder: ${this.placeholder}\
         \n\t placeholder.length: ${this.placeholder.length}\
         \n\t indexStart: ${this.indexStart}\
         \n\t indexEnd: ${this.indexEnd}\
         \n\t length: ${this.length}`)
-    }
   }
 }
 

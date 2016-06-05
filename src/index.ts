609d2c25d208bf39e000ee3284ddf610a55be3ce
nskazki <nskazki@gmail.com>
2016-06-05 04:00:39 +0300

    added support fot the number of placeholders are not match with number of replacers

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 'use strict'
 
 import handlers from './handlers'
-import { IMurky } from './interfaces'
+import { IMurky, IPInfo } from './interfaces'
 import { inspect } from 'util'
 import { isString, fill, toArray } from 'lodash'
 import uncolor = require('uncolor')
@@ -9,6 +9,12 @@ import supportsColor = require('supports-color')
 import assert = require('assert')
 
 const murky: IMurky = function(fmtStr, ...rawReplacers) {
+  // rawReplacers & fmtStr may be redefined in case if fmtStr is missed
+  // look "like util.format without fmtStr" code block
+
+  // fmtStr may be extended in case if count(placeholders(fmtStr)) < rawReplacers.length
+  // look "check psInfo and rawReplacer size" code block size
+
   // like util.format without arguments
   if (arguments.length === 0)
     return ''
@@ -20,55 +26,22 @@ const murky: IMurky = function(fmtStr, ...rawReplacers) {
   }
 
   // get placeholders info
-  // placeholder finders may has problems with %%
-  // tmpStr used only to get info about placeholders
-  const tmpStr = fmtStr.replace(/%%/g, '  ')
-  const psInfo = handlers.findAll(tmpStr)
+  // psInfo may be
+  // - truncated in case if psInfo.length gt rawReplacers.length
+  // - extended  in case if psInfo.length lt rawReplacer.length
+  let psInfo = extractAllPInfo(fmtStr)
 
-  // check placeholders info
-  const psInfoOrderErrors = psInfo.map((pInfoA, indexA) => {
-    return psInfo.slice(0, indexA).map((pInfoB, indexB) => {
-      return { pInfoB, indexB }
-    }).filter(({ pInfoB }) => {
-      return pInfoA !== pInfoB
-    }).filter(({ pInfoB }) => {
-      return pInfoA.indexStart <= pInfoB.indexStart
-    }).map(({ pInfoB, indexB }) => {
-      return `wrong order of found placeholders: A should be placed after B:\
-        \n\t A index: ${indexA}\
-        \n\t B index: ${indexB}\
-        \n\t A data: ${inspect(pInfoA)}\
-        \n\t B data: ${inspect(pInfoB)}`
-    })
-  }).reduce((acc, part) => acc.concat(part), [])
-
-  const psInfoCrossErrors = psInfo.map((pInfoA, indexA) => {
-    return psInfo.map((pInfoB, indexB) => {
-      return { pInfoB, indexB }
-    }).filter(({ pInfoB }) => {
-      return pInfoA !== pInfoB
-    }).filter(({ pInfoB }) => {
-      return !(false
-        || (pInfoA.indexEnd < pInfoB.indexStart)
-        || (pInfoB.indexEnd < pInfoA.indexStart))
-    }).map(({ pInfoB, indexB }) => {
-      return `placeholder A should not cross placeholder B:\
-        \n\t A index: ${indexA}\
-        \n\t B index: ${indexB}\
-        \n\t A data: ${inspect(pInfoA)}\
-        \n\t B data: ${inspect(pInfoB)}`
-    })
-  }).reduce((acc, part) => acc.concat(part), [])
-
-  const psInfoErrors = [ ...psInfoOrderErrors, ...psInfoCrossErrors ]
-  assert.ok(psInfoErrors.length === 0, `murky: internal problem!\n ${psInfoErrors.join('\n ')}`)
-
-  // check user input: rawReplacer
-  if (psInfo.length !== rawReplacers.length)
-    throw new Error(`murky: input problem: the number of found placeholders does not match the number of replacers!\
-      \n\t placeholders.length: ${psInfo.length}\
-      \n\t replacers.length: ${rawReplacers.length}\
-      \n\t placeholders info: \n${inspect(psInfo)}\n`)
+  // check psInfo and rawReplacer size
+  if (psInfo.length > rawReplacers.length) {
+    // murky('%s %s', 123)
+    psInfo = psInfo.slice(0, rawReplacers.length)
+  } else if (psInfo.length < rawReplacers.length) {
+    // murky('%s', 123, 345)
+    const missedCount = rawReplacers.length - psInfo.length
+    const missedPlaceholders = fill(new Array(missedCount), '%s').join(' ')
+    fmtStr = fmtStr + ' ' + missedPlaceholders
+    psInfo = extractAllPInfo(fmtStr)
+  }
 
   // calc result string
   let replacers: Array<string> = []
@@ -138,6 +111,54 @@ const murky: IMurky = function(fmtStr, ...rawReplacers) {
 
   // return result string
   return resStr
+}
+
+// helpers
+function extractAllPInfo(fmtStr: string): Array<IPInfo> {
+  // placeholder finders may has problems with %%
+  // tmpStr used only to get info about placeholders
+  const tmpStr = fmtStr.replace(/%%/g, '  ')
+  const psInfo = handlers.findAll(tmpStr)
+
+  // check placeholders info
+  const psInfoOrderErrors = psInfo.map((pInfoA, indexA) => {
+    return psInfo.slice(0, indexA).map((pInfoB, indexB) => {
+      return { pInfoB, indexB }
+    }).filter(({ pInfoB }) => {
+      return pInfoA !== pInfoB
+    }).filter(({ pInfoB }) => {
+      return pInfoA.indexStart <= pInfoB.indexStart
+    }).map(({ pInfoB, indexB }) => {
+      return `wrong order of found placeholders: A should be placed after B:\
+        \n\t A index: ${indexA}\
+        \n\t B index: ${indexB}\
+        \n\t A data: ${inspect(pInfoA)}\
+        \n\t B data: ${inspect(pInfoB)}`
+    })
+  }).reduce((acc, part) => acc.concat(part), [])
+
+  const psInfoCrossErrors = psInfo.map((pInfoA, indexA) => {
+    return psInfo.map((pInfoB, indexB) => {
+      return { pInfoB, indexB }
+    }).filter(({ pInfoB }) => {
+      return pInfoA !== pInfoB
+    }).filter(({ pInfoB }) => {
+      return !(false
+        || (pInfoA.indexEnd < pInfoB.indexStart)
+        || (pInfoB.indexEnd < pInfoA.indexStart))
+    }).map(({ pInfoB, indexB }) => {
+      return `placeholder A should not cross placeholder B:\
+        \n\t A index: ${indexA}\
+        \n\t B index: ${indexB}\
+        \n\t A data: ${inspect(pInfoA)}\
+        \n\t B data: ${inspect(pInfoB)}`
+    })
+  }).reduce((acc, part) => acc.concat(part), [])
+
+  const psInfoErrors = [ ...psInfoOrderErrors, ...psInfoCrossErrors ]
+  assert.ok(psInfoErrors.length === 0, `murky: internal problem!\n ${psInfoErrors.join('\n ')}`)
+
+  return psInfo
 }
 
 // it's required for the *.d.ts formation
